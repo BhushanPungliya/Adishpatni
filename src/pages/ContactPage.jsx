ebcef0baed32a95fce5e0706cd3294a5a8f7469b
Mohammad Ahmad <104555382+ahmadparizaad@users.noreply.github.com>
2026-01-23 17:37:19 +0530

    fix: add additional office addresses to ContactPage

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -24,6 +24,26 @@ function ContactPage() {
               72, Saket Nagar <br /> Indore Madhya Pradesh 452 018
             </a>
           </p>
+          <p className="office-text">
+            <a
+              style={{ textDecoration: "none", color: "#fff" }}
+              href="https://www.google.com/maps/search/?api=1&query=72%20Saket%20Nagar%20Indore%20452018"
+              target="_blank"
+              rel="noreferrer"
+            >
+              72, Saket Nagar <br /> Indore Madhya Pradesh 452 018
+            </a>
+          </p>
+          <p className="office-text">
+            <a
+              style={{ textDecoration: "none", color: "#fff" }}
+              href="https://www.google.com/maps/search/?api=1&query=R.R+Heights+B.G+Road+Bangalore+560076"
+              target="_blank"
+              rel="noreferrer"
+            >
+              R.R Heights, B.G Road <br /> Bangalore 560 076
+            </a>
+          </p>
           <p className="office-telephone">
             <a href="tel:+918550811105" style={{ textDecoration: "none", color: "inherit" }}>
               Contact : +91 85508 11105
